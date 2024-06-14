f231ba1cb4cbde2f3c8d6d21ed42fbb67b759557
skitchin <scott@kitchin.com>
2024-06-14 12:38:38 -0400

    Initial Project

---
+++ src/main/java/com/kitchin/persistence/UserController.java
@@ -0,0 +1,23 @@
+package com.kitchin.persistence;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+}
