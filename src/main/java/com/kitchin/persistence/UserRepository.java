042620435cdefbae9f899f39641242d5ad16b3eb
skitchin <scott@kitchin.com>
2024-06-14 12:39:55 -0400

    Initial Project

---
+++ src/main/java/com/kitchin/persistence/UserRepository.java
@@ -0,0 +1,5 @@
+package com.kitchin.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {}
---
+++ src/main/java/com/kitchin/persistence/UserService.java
@@ -0,0 +1,21 @@
+package com.kitchin.persistence;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
